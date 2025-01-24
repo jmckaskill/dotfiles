1353f43d9969ee719408638fb1d59402501cd4ed
James McKaskill <james@foobar.co.nz>
2025-01-24 09:17:54 -0700

    updates

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -12,4 +12,14 @@ if status is-interactive
 		end
 	end
 	bind \cj 'fzf_jj_edit'
+
+
+	function y
+		set tmp (mktemp -t "yazi-cwd.XXXXXX")
+		yazi $argv --cwd-file="$tmp"
+		if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+			builtin cd -- "$cwd"
+		end
+		rm -f -- "$tmp"
+	end
 end
